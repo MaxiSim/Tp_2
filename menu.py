d5f69aaf1eb5c8c03c66b2db7f810ef2f3352246
Bruno Castagnino Rossi <bcastagninorossi@udesa.edu,ar>
2022-04-11 17:30:32 -0300

    main menu

---
+++ menu.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+def menu ()->int:
+    '''Selects options from prompt and returns game mode or quit
+    '''
+    mode = input("MENU\n1. Play\n2. See words list\n3. Quit\nSelect at most one option >").lower()
+    while mode != '1' and mode != 'play' and mode != '2' and mode != 'see words list' and mode != '3' and mode != 'quit':
+        mode = input('Ingresó un modo inválido, intnte nuevamente > ')
+    if  mode == '1' or mode == 'play':
+        hangman_menu()
+    elif mode == '2' or mode =='see words list':
+        see_list()
+    elif mode == 3 or mode == 'quit':
+        quit()
