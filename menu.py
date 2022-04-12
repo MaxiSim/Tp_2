6f093e2405e7b2b4e778b864fa3b39d26264d990
MaxiSim <101142571+MaxiSim@users.noreply.github.com>
2022-04-12 13:37:40 -0300

    Menu generalizado
    funcion menu reusable

--- menu.py
+++ menu.py
@@ -1,36 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from see_list import see_list
+from human_hangman import human_hangman
+from computer_hangman import computer_hangman
 
-def menu ():
-    '''Selects options from prompt and returns game mode or quit
-    '''
-    run = True
-    while run:
-        mode = input("MENU\n1. Play\n2. See words list\n3. Quit\nSelect one option > ").lower()
-        while mode != '1' and mode != 'play' and mode != '2' and mode != 'see words list' and mode != '3' and mode != 'quit':
-            mode = input('Ingresó un modo inválido, intnte nuevamente > ')
-        if  mode == '1' or mode == 'play':
-            play()
-        elif mode == '2' or mode =='see words list':
-            see_list()
-            continue
-        elif mode == '3' or mode == 'quit':
-            run = False
+def go_back():
+      print('back')
+
+def menu_ppal():
+      menu('MENU', list(funciones_menu.keys()))
 
 def play():
-  select_option = input('''Please select an option to continue:\n1. Human Hangman\n2. Computer Hangman\n3. Go Back\n>''').lower()
+    funcplay = menu('Please select a gamemode or go back to the main menu.', list(funciones_menu_play.keys()))
+    funciones_menu_play[funcplay]()
 
-  while select_option != '1' and select_option != 'human hangman' and select_option != '2' and select_option != 'computer hangman' and select_option != '3' and select_option != 'go back':
-        select_option = input('Invalid input, please try again > ')
-        
-  if select_option == '1' or select_option == 'human hangman':
-    print('human')
-    #human_hangman()
-  elif select_option == '2' or select_option == 'computer hangman':
-    print('computer')
-    #computer_hangman()
-  else:
-    menu()
-    #go_back()
+def menu(prompt: str, opciones: list) -> str :
+    print(prompt)
+    for i, opcion in enumerate(opciones):
+       print(f'{i + 1}. {opcion}')
+  
+    return opciones[int(input("> ")) - 1] 
+  
+funciones_menu = {
+  'Play': play,
+  'See list': see_list, 
+  'Quit': quit
+}            
+funciones_menu_play = {
+  'Human Hangman': human_hangman,
+  'Computer Hangman': computer_hangman,
+  'Go Back': go_back
+}
+  
+func = menu('MENU', list(funciones_menu.keys()))
+funciones_menu[func]()
+
+
 
